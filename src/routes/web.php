d1bfa23def60e2f4eed2194a3da230dd4b405ada
Mark Bailey <mabailey78@yahoo.co.uk>
2017-09-08 17:18:49 +0100

    Updated routes to match new db structure

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,37 @@
 <?php
 
-Route::group(['domain' => config('pagebuilder.domain')], function(){
+Route::group(['domain' => config('pagebuilder.domain')], function() {
     /*
     *   HTTP Routes
     */
-    // Route::get('/', array('as' => 'pagebuilder.dashboard', 'uses' => 'PageBuilderController@dashboard'));
-    Route::get('/editor/page/{id?}', array('as' => 'pagebuilder.editor.page', 'uses' => 'PageBuilderController@pageEditor'));
+
+    Route::group(['prefix' => '/editor'], function() {
+        Route::get('/{type}/{id}', array('as' => 'pagebuilder.editor', 'uses' => 'PageBuilderController@editor'))
+        ->where('type', 'block|layout|page');
+    });
 
     /*
     *   Ajax Routers
     */
-    Route::group(['prefix' => '/ajax'], function(){
-        Route::get('/getblocks', array('as' => 'pagebuilder.ajax.getblocks', 'uses' => 'PageBuilderController@getBlocks'));
+
+    Route::group(['prefix' => '/ajax'], function() {
+        /** GET **/
+        Route::group(['prefix' => '/get'], function() {
+            /** BLOCKS **/
+            Route::get('/{type}/{userDefined?}', array('as' => 'pagebuilder.ajax.get.blocks', 'uses' => 'PageBuilderController@getBlocks'))
+            ->where('type', 'blocks|layouts');
+        });
+
+        /** LOAD **/
+        Route::group(['prefix' => '/load'], function() {
+            Route::get('/{type}/{id}', array('as' => 'pagebuilder.ajax.load', 'uses' => 'PageBuilderController@load'))
+            ->where('type', 'block|layout|page');
+        });
+
+        /** STORE **/
+        Route::group(['prefix' => '/store'], function() {
+            Route::post('/{type}/{id}', array('as' => 'pagebuilder.ajax.store', 'uses' => 'PageBuilderController@store'))
+            ->where('type', 'block|layout|page');
+        });
     });
 });
