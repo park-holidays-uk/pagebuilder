d48db602bd1c16bf0a02ab9fd8d4ab52dec25661
Mark Bailey <mabailey78@yahoo.co.uk>
2017-09-08 17:21:17 +0100

    Initial commit of loading blocks on events

--- src/resources/assets/js/grapejs-blocks.js
+++ src/resources/assets/js/grapejs-blocks.js
@@ -4,62 +4,105 @@ grapesjs.plugins.add('blocks', (editor, options) => {
     var opt = options || {};
     var config = editor.getConfig();
     var blockManager = editor.BlockManager;
+    /** DOM WRAPPER **/
+    var domComponents = editor.DomComponents;
+    var wrapper = domComponents.getWrapper();
 
-    $.ajax({
-        url: '/ajax/getblocks',
-    }).done(function(_data) {
-        var _blocks = JSON.parse(_data);
-        console.log(_blocks);
+    /*
+     *   Functions 
+     */
 
-        _.forEach(_blocks, function(_block) {
-            blockManager.add(_block.block_id, {
-                label: _block.label,
-                category: _block.category,
-                content: _block.content,
-                attributes: _block.attributes,
+    var loadBlocks = function(_type = 'blocks', _userDefined = true) {
+        _.forEach(blockManager.getAll().models, function(_block) {
+            console.log(_block);
+            blockManager.remove(_block);
+        });
+
+        console.log('Blocks', blockManager.getAll());
+
+        $.ajax({
+            url: '/ajax/get/' + _type + '/' + _userDefined,
+        }).done(function(_data) {
+            var _blocks = JSON.parse(_data);
+
+            _.forEach(_blocks, function(_block) {
+                blockManager.add(_block.block_id, {
+                    label: _block.label,
+                    category: _block.category,
+                    content: _block.content,
+                    attributes: JSON.parse(_block.attributes),
+                });
             });
         });
+    };
+
+    /*
+     *   Events
+     */
+
+    editor.on('load', function() {
+        if (opt.mode == 'page') {
+            if (editor.getComponents().length == 0) {
+                loadBlocks('layouts');
+            } else {
+                loadBlocks();
+            }
+        } else {
+            loadBlocks('blocks', false);
+        }
     });
 
-    //     blockManager.add('layout-one', {
-    //         label: '1 Column',
-    //         category: "Layouts",
-    //         content: {
-    //             content: atob('PGRpdiBjbGFzcz0iZ3JpZCI+DQogICAgPGRpdiBjbGFzcz0iY29sLTEyIj4NCiAgICANCiAgICA8L2Rpdj4NCjwvZGl2Pg=='),
-    //             stylable: false,
-    //             copyable: false,
-    //             removable: false, // Once inserted it can't be removed
-    //         },
-    //         attributes: {
-    //             class: "gjs-fonts gjs-f-b1"
-    //         },
-    //     });
+    editor.on('block:drag:stop', function(model) {
+        if (opt.mode == 'page') {
+            if (wrapper.attributes.droppable) {
+                loadBlocks();
+                wrapper.attributes.droppable = false;
+                console.log('Wrapper', wrapper);
+            }
+        }
+    });
 
-    //     blockManager.add('layout-two', {
-    //         label: '2 Columns 6/6',
-    //         category: "Layouts",
-    //         content: {
-    //             content: atob('PGRpdiBjbGFzcz0iZ3JpZCI+DQogICAgPGRpdiBjbGFzcz0iY29sLTYiPg0KICAgIA0KICAgIDwvZGl2Pg0KICAgIDxkaXYgY2xhc3M9ImNvbC02Ij4NCiAgICANCiAgICA8L2Rpdj4NCjwvZGl2Pg=='),
-    //             stylable: false,
-    //             copyable: false,
-    //             removable: false,
-    //         },
-    //         attributes: {
-    //             class: "gjs-fonts gjs-f-b2"
-    //         },
-    //     });
+    // blockManager.add('container', {
+    //     label: 'Container',
+    //     category: "Basic",
+    //     content: atob('PGRpdiBjbGFzcz0iY29udGFpbmVyIiBkYXRhLWdqcy1jdXN0b20tbmFtZT0iQ29udGFpbmVyIj48L2Rpdj4='),
+    //     attributes: {
+    //         class: "gjs-fonts gjs-f-b1 gjs-block"
+    //     },
+    // });
 
-    //     blockManager.add('layout-three', {
-    //         label: '2 Columns 8/4',
-    //         category: "Layouts",
-    //         content: {
-    //             content: atob('PGRpdiBjbGFzcz0iZ3JpZCI+DQogICAgPGRpdiBjbGFzcz0iY29sLTgiPg0KICAgIA0KICAgIDwvZGl2Pg0KICAgIDxkaXYgY2xhc3M9ImNvbC00Ij4NCiAgICANCiAgICA8L2Rpdj4NCjwvZGl2Pg=='),
-    //             stylable: false,
-    //             copyable: false,
-    //             removable: false, // Once inserted it can't be removed
-    //         },
-    //         attributes: {
-    //             class: "gjs-fonts gjs-f-b37"
-    //         },
-    //     });
+    // blockManager.add('container-fluid', {
+    //     label: 'Fluid Container',
+    //     category: "Basic",
+    //     content: atob('PGRpdiBjbGFzcz0iY29udGFpbmVyLWZsdWlkIiBkYXRhLWdqcy1jdXN0b20tbmFtZT0iQ29udGFpbmVyIj48L2Rpdj4='),
+    //     attributes: {
+    //         class: "gjs-fonts gjs-f-b1 gjs-block"
+    //     },
+    // });
+
+    // blockManager.add('image', {
+    //     label: 'Image',
+    //     category: "Basic",
+    //     content: atob('PGltZyBzcmM9Imh0dHA6Ly9wbGFjZWhvbGQuaXQvNDUweDM1MD90ZXh0PUltYWdlK1BsYWNlaG9sZGVyIiBhbHQ9IiIgY2xhc3M9ImltZy1yZXNwb25zaXZlIgogICAgIGRhdGEtZ2pzLXN0eWxhYmxlPSJmYWxzZSIKICAgICBkYXRhLWdqcy1yZXNpemFibGU9ImZhbHNlIiAvPg=='),
+    //     attributes: {
+    //         class: "gjs-fonts gjs-f-image"
+    //     },
+    // });
+
+    // blockManager.add('grid-three-column', {
+    //     label: '3 Columns',
+    //     category: "Grid",
+    //     content: atob('PGRpdiBjbGFzcz0iZ3JpZC0zX21kLTJfeHMtMSIgCiAgICAgZGF0YS1nanMtY3VzdG9tLW5hbWU9IlJvdyIgCiAgICAgZGF0YS1nanMtZHJvcHBhYmxlPSIuY29sdW1uIgogICAgIGRhdGEtZ2pzLWNvcHlhYmxlPSJmYWxzZSIKICAgICBkYXRhLWdqcy1zdHlsYWJsZT0iZmFsc2UiCj4KICAgPGRpdiBjbGFzcz0iY29sIiAKICAgICAgICBkYXRhLWdqcy1jdXN0b20tbmFtZT0iQ29sdW1uIiAKICAgICAgICBkYXRhLWdqcy1yZW1vdmFibGU9ImZhbHNlIgogICAgICAgIGRhdGEtZ2pzLWNvcHlhYmxlPSJmYWxzZSIKICAgICAgICBkYXRhLWdqcy1kcmFnZ2FibGU9ImZhbHNlIgogICAgICAgIGRhdGEtZ2pzLXN0eWxhYmxlPSJmYWxzZSIKICAgPjwvZGl2PgogICA8ZGl2IGNsYXNzPSJjb2wiIAogICAgICAgIGRhdGEtZ2pzLWN1c3RvbS1uYW1lPSJDb2x1bW4iIAogICAgICAgIGRhdGEtZ2pzLXJlbW92YWJsZT0iZmFsc2UiCiAgICAgICAgZGF0YS1nanMtY29weWFibGU9ImZhbHNlIgogICAgICAgIGRhdGEtZ2pzLWRyYWdnYWJsZT0iZmFsc2UiCiAgICAgICAgZGF0YS1nanMtc3R5bGFibGU9ImZhbHNlIgogICA+PC9kaXY+CiAgIDxkaXYgY2xhc3M9ImNvbCIgCiAgICAgICAgZGF0YS1nanMtY3VzdG9tLW5hbWU9IkNvbHVtbiIgCiAgICAgICAgZGF0YS1nanMtcmVtb3ZhYmxlPSJmYWxzZSIKICAgICAgICBkYXRhLWdqcy1jb3B5YWJsZT0iZmFsc2UiCiAgICAgICAgZGF0YS1nanMtZHJhZ2dhYmxlPSJmYWxzZSIKICAgICAgICBkYXRhLWdqcy1zdHlsYWJsZT0iZmFsc2UiCiAgICA+PC9kaXY+CjwvZGl2Pg=='),
+    //     // content: {
+    //     //     content: atob('PGRpdiBjbGFzcz0iZ3JpZCIgCiAgICAgZGF0YS1nanMtY3VzdG9tLW5hbWU9IlJvdyIgCiAgICAgZGF0YS1nanMtZHJvcHBhYmxlPSIuY29sdW1uIgogICAgIGRhdGEtZ2pzLWNvcHlhYmxlPSJmYWxzZSIKPgogICA8ZGl2IGNsYXNzPSJjb2wtNCIgCiAgICAgICAgZGF0YS1nanMtY3VzdG9tLW5hbWU9IkNvbHVtbiIgCiAgICAgICAgZGF0YS1nanMtcmVtb3ZhYmxlPSJmYWxzZSIKICAgICAgICBkYXRhLWdqcy1jb3B5YWJsZT0iZmFsc2UiCiAgICAgICAgZGF0YS1nanMtZHJhZ2dhYmxlPSJmYWxzZSIKICAgPjwvZGl2PgogICA8ZGl2IGNsYXNzPSJjb2wtNCIgCiAgICAgICAgZGF0YS1nanMtY3VzdG9tLW5hbWU9IkNvbHVtbiIgCiAgICAgICAgZGF0YS1nanMtcmVtb3ZhYmxlPSJmYWxzZSIKICAgICAgICBkYXRhLWdqcy1jb3B5YWJsZT0iZmFsc2UiCiAgICAgICAgZGF0YS1nanMtZHJhZ2dhYmxlPSJmYWxzZSIKICAgPjwvZGl2PgogICA8ZGl2IGNsYXNzPSJjb2wtNCIgCiAgICAgICAgZGF0YS1nanMtY3VzdG9tLW5hbWU9IkNvbHVtbiIgCiAgICAgICAgZGF0YS1nanMtcmVtb3ZhYmxlPSJmYWxzZSIKICAgICAgICBkYXRhLWdqcy1jb3B5YWJsZT0iZmFsc2UiCiAgICAgICAgZGF0YS1nanMtZHJhZ2dhYmxlPSJmYWxzZSIKICAgID48L2Rpdj4KPC9kaXY+'),
+    //     //     stylable: false,
+    //     //     copyable: false,
+    //     //     removable: false, // Once inserted it can't be removed
+    //     // },
+    //     attributes: {
+    //         class: "gjs-fonts gjs-f-b3"
+    //     },
+    // });
+
+
 });
--- src/resources/assets/js/grapejs-preset-webpage.js
+++ src/resources/assets/js/grapejs-preset-webpage.js
@@ -1,198 +1,43 @@
 /*export default*/
 grapesjs.plugins.add('preset-webpage', (editor, options) => {
 
+    /*
+     *   VARIABLES
+     */
+
     var opt = options || {};
     var config = editor.getConfig();
-    var pfx = editor.getConfig().stylePrefix;
+    var stylePrefix = editor.getConfig().stylePrefix;
+
+    /** MANAGERS **/
+    var assetManager = editor.AssetManager;
+    // const amConfig = editor.AssetManager.getConfig();
+
+    // amConfig.upload = 0;
+    // amConfig.dropzone = 0;
+
+    /** MODAL **/
     var modal = editor.Modal;
 
-    config.showDevices = 0;
-
-    /** DOM Wrapper **/
+    /** DOM WRAPPER **/
     var domComponents = editor.DomComponents;
     var wrapper = domComponents.getWrapper();
-    // wrapper.set('attributes', { 'data-gjs-droppable': '.column' });
+    // wrapper.attributes.stylable = false;
+    // config.showDevices = 0;
 
-    /** IMPORTER **/
-    var codeViewer = editor.CodeManager.getViewer('CodeMirror').clone();
-    var container = document.createElement('div');
-    var btnImp = document.createElement('button');
 
-    // Init import button
-    btnImp.innerHTML = 'Import';
-    btnImp.className = pfx + 'btn-prim ' + pfx + 'btn-import';
-    btnImp.onclick = function() {
-        var code = codeViewer.editor.getValue();
-        editor.DomComponents.getWrapper().set('content', '');
-        editor.setComponents(code.trim());
-        modal.close();
-    };
 
-    /** COMMANDS **/
-    var cmdm = editor.Commands;
 
-    cmdm.add('set-device-desktop', {
-        run: function(editor) {
-            editor.setDevice('Desktop');
-        }
-    });
-    cmdm.add('set-device-tablet', {
-        run: function(editor) {
-            editor.setDevice('Tablet');
-        }
-    });
-    cmdm.add('set-device-mobile', {
-        run: function(editor) {
-            editor.setDevice('Mobile portrait');
-        }
+    /*
+     *   EVENTS
+     */
+
+    editor.on('load', function() {
+        console.log('Components', editor.getComponents());
+        console.log('Wrapper', wrapper);
+        console.log('Editor', editor);
+
+        console.log('Panels', editor.Panels.getPanels());
     });
 
-    cmdm.add('undo', {
-        run: function(editor, sender) {
-            sender.set('active', 0);
-            editor.UndoManager.undo(1);
-        }
-    });
-
-    cmdm.add('redo', {
-        run: function(editor, sender) {
-            sender.set('active', 0);
-            editor.UndoManager.redo(1);
-        }
-    });
-
-    cmdm.add('html-import', {
-        run: function(editor, sender) {
-            sender && sender.set('active', 0);
-
-            var modalContent = modal.getContentEl();
-            var viewer = codeViewer.editor;
-            modal.setTitle('Import Template');
-
-            // Init code viewer if not yet instantiated
-            if (!viewer) {
-                var txtarea = document.createElement('textarea');
-                var labelEl = document.createElement('div');
-                labelEl.className = pfx + 'import-label';
-                labelEl.innerHTML = 'Paste here your HTML/CSS and click Import';
-                container.appendChild(labelEl);
-                container.appendChild(txtarea);
-                container.appendChild(btnImp);
-                codeViewer.init(txtarea);
-                viewer = codeViewer.editor;
-            }
-
-            modal.setContent('');
-            modal.setContent(container);
-            codeViewer.setContent(
-                '<div class="txt-red">Hello world!</div>' +
-                '<style>\n.txt-red {color: red;padding: 30px\n}</style>'
-            );
-            modal.open();
-            viewer.refresh();
-        }
-    });
-
-    // Init code viewer
-    codeViewer.set({
-        codeName: 'htmlmixed',
-        theme: opt.codeViewerTheme || 'hopscotch',
-        readOnly: 0
-    });
-
-    /** Buttons **/
-    var pnm = editor.Panels;
-    pnm.addPanel({ id: 'options' });
-
-    pnm.addButton('options', [{
-            id: 'sw-visibility',
-            className: 'fa fa-square-o',
-            command: 'sw-visibility',
-            attributes: { title: 'View Components' },
-            active: true
-        },
-        {
-            id: 'preview',
-            className: 'fa fa-eye',
-            command: 'preview',
-            attributes: { title: 'Preview' }
-        },
-        {
-            id: 'fullscreen',
-            className: 'fa fa-arrows-alt',
-            command: 'fullscreen',
-            attributes: { title: 'Fullscreen' }
-        },
-        {
-            id: 'export-template',
-            className: 'fa fa-code',
-            command: 'export-template',
-            attributes: { title: 'View code' }
-        },
-        {
-            id: 'undo',
-            className: 'fa fa-undo icon-undo',
-            command: 'undo',
-            attributes: { title: 'Undo (CTRL/CMD + Z)' }
-        },
-        {
-            id: 'redo',
-            className: 'fa fa-repeat icon-redo',
-            command: 'redo',
-            attributes: { title: 'Redo (CTRL/CMD + SHIFT + Z)' }
-        },
-        {
-            id: 'import',
-            className: 'fa fa-download',
-            command: 'html-import',
-            attributes: { title: 'Import' }
-        }
-    ]);
-
-    /** Device Buttons **/
-    var panelDevices = pnm.addPanel({ id: 'devices-c' });
-    var deviceBtns = panelDevices.get('buttons');
-    deviceBtns.add([{
-            id: 'deviceDesktop',
-            command: 'set-device-desktop',
-            className: 'fa fa-desktop',
-            attributes: { 'title': 'Desktop' },
-            active: 1,
-        },
-        {
-            id: 'deviceTablet',
-            command: 'set-device-tablet',
-            className: 'fa fa-tablet',
-            attributes: { 'title': 'Tablet' },
-        },
-        {
-            id: 'deviceMobile',
-            command: 'set-device-mobile',
-            className: 'fa fa-mobile',
-            attributes: { 'title': 'Mobile' },
-        }
-    ]);
-
-    /** Views  */
-    pnm.addPanel({ id: 'views' });
-    pnm.addPanel({ id: 'views-container' });
-    // cmdm.get('open-blocks');
-
-    pnm.addButton('views', [{
-            id: 'open-layers',
-            className: 'fa fa-bars',
-            command: 'open-layers',
-            attributes: { title: 'Open Layer Manager' },
-            active: false,
-        },
-        {
-            id: 'open-blocks',
-            className: 'fa fa-th-large',
-            command: 'open-blocks',
-            attributes: { title: 'Open Blocks' },
-            active: true,
-        }
-    ]);
-
-    console.log(pnm);
 });
