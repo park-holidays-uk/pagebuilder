e4febeddab3e6361273a8954f25628543f2e59b2
Mark Bailey <mabailey78@yahoo.co.uk>
2017-10-25 14:49:05 +0100

    Moved lodash js  out of vendor folder as being ignored

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -22,7 +22,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     
     <!-- LOCAL -->
-    <script type="text/javascript" src="{{ asset('parkholidays/pagebuilder/js/vendor/lodash.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('parkholidays/pagebuilder/js/lodash.js') }}"></script>
     <script type="text/javascript" src="{{ asset('parkholidays/pagebuilder/js/grapes.min.js') }}"></script>
     
     <!-- Custom Scripts -->
